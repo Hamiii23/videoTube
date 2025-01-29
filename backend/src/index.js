47e553562c2508f776e27279caf1d2fe34d1ddca
Hami <23hamiid@gmail.com>
2025-01-29 21:01:00 +0530

    utils added and app

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,23 @@
 import 'dotenv/config'
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
-connectDB();
+const port = process.env.PORT || 8000
+
+connectDB()
+.then( () => {
+    app.on("ERROR ", (error) => {
+        console.log("ERROR: ", error);
+        throw error;
+    });
+    app.listen(port, () => {
+        console.log(`Server is running on port: ${port}`);
+        
+    })
+})
+.catch((err) => {
+    console.error("MongoDB Connection Failed: ", err);
+})
 
 // import express from 'express';
 // const app = express();
