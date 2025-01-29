0e9a6c8c919d18301e72ceb6bf61503749c7111d
Hami <23hamiid@gmail.com>
2025-01-29 21:40:57 +0530

    fixed an error

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser'
 
+const app = express();
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
@@ -12,6 +13,5 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static('public'));
 app.use(cookieParser());
 
-const app = express();
 
 export { app }
