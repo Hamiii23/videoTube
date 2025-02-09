b4dbeb7d9be51afdc169d6a925e53d4b44ebde2e
Hami <23hamiid@gmail.com>
2025-02-09 21:06:37 +0530

    added update logic

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { registerUser, loginUser, logOutUser, refreshAccessToken } from '../controllers/user.controller.js';
+import { registerUser, loginUser, logOutUser, refreshAccessToken, changeCurrentPassword, updateAccountDetails, updateAvatar, updateCoverImage } from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 
@@ -24,5 +24,31 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT,logOutUser);
 
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").put(verifyJWT, changeCurrentPassword);
+
+router.route("/update-details").put(verifyJWT, updateAccountDetails);
+
+router.route("/update-avatar").put(
+    verifyJWT, 
+    upload.fields(
+        {
+            name: "avatar",
+            maxCount: 1
+        }
+    ), updateAvatar
+);
+
+router.route("/update-cover").put(
+    verifyJWT, 
+    upload.fields(
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ), updateCoverImage
+);
+
+
 
 export default router;
