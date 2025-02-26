ed1a7f9d6aacc10a5046f43b356e994e71f47f99
Hami <23hamiid@gmail.com>
2025-02-26 13:09:37 +0530

    add validation for user and tweet IDs in tweet controller

--- backend/src/controllers/tweet.controller.js
+++ backend/src/controllers/tweet.controller.js
@@ -19,7 +19,6 @@ const createTweet = asyncHandler(async (req, res) => {
         owner: user._id
     });
     
-    // const createdTweet = await Tweet.findById(tweet._id);
     
     if(!tweet) {
         throw new ApiError(500, "Something went wrong while posting the tweet");
@@ -32,20 +31,56 @@ const createTweet = asyncHandler(async (req, res) => {
     );
 });
 
-const getUserTweets = asyncHandler(async (req, res) => {});
+const getUserTweets = asyncHandler(async (req, res) => {
+    const {userId} = req.params;
+
+    const validUserId = isValidObjectId(userId);
+
+    if(!validUserId) {
+        throw new ApiError(400, "Invalid User Id");
+    };
+
+    const userTweets = await Tweet.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $project: {
+                content: 1,
+                createdAt: 1,
+                updatedAt: 1
+            }
+        }
+    ]);
+
+    if(!userTweets) {
+        throw new ApiError(500, "Something went wrong while looking for user tweets");
+    };
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, userTweets, "User tweets fetched successfully")
+    );
+});
 
 const updateTweets = asyncHandler(async (req, res) => {
     const { tweetId } = req.params;
     const { content } = req.body;
     
-    if(!tweetId) {
+
+    const validTweetId = isValidObjectId(tweetId);
+    
+    if(!validTweetId) {
         throw new ApiError(400, "Invalid tweet Id");
     };
     
     if(!content) {
         throw new ApiError(400, "Tweet body is required");
     };
-
+    
     const updatedTweet = await Tweet.findByIdAndUpdate(
         tweetId,
         {
@@ -57,11 +92,11 @@ const updateTweets = asyncHandler(async (req, res) => {
             new: true
         }
     );
-
+    
     if (!updatedTweet) {
         throw new ApiError(500, "Something went wrong while updating the tweet")
     };
-
+    
     return res
     .status(200)
     .json(
@@ -71,8 +106,10 @@ const updateTweets = asyncHandler(async (req, res) => {
 
 const deleteTweets = asyncHandler(async (req, res) => {
     const { tweetId } = req.params;
+    
+    const validTweetId = isValidObjectId(tweetId);
 
-    if(!tweetId) {
+    if(!validTweetId) {
         throw new ApiError(400, "Invalid tweet Id");
     };
 
